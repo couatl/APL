1bc39b34c5bd5e6a38dd7e2721379efa6bd7fe89
Svoykina N <toscharlina@yandex.ru>
2016-10-06 21:50:32 +0300

    Update my_test.cpp

--- Lab2/tests/my_test.cpp
+++ Lab2/tests/my_test.cpp
@@ -1,45 +1,8 @@
 #include "../Lab2/include/prime_check.h"
 #include "catch.hpp"
 
-SCENARIO("if number is negative")
-{
-    GIVEN("negative number")
-    {
-        int k = -100;
-        WHEN("checking on primeness")
-        {
-            THEN("return false")
-            {
-                REQUIRE(prime_check(k) == false);
-            }
-        }
-    }
-}
-SCENARIO("if number is prime")
-{
-    GIVEN("prime number")
-    {
-        int k = 7;
-        WHEN("checking on primeness")
-        {
-            THEN("return true")
-            {
-                REQUIRE(prime_check(k) == true);
-            }
-        }
-    }
-}
-SCENARIO("if number isn't prime")
-{
-    GIVEN("not prime number")
-    {
-        int k = 10200;
-        WHEN("checking on primeness")
-        {
-            THEN("return false")
-            {
-                REQUIRE(prime_check(k) == false);
-            }
-        }
-    }
+TEST_CASE( "Checking on primeness", "[number]" ) {
+    REQUIRE( prime_check(-100) == false );
+    REQUIRE( prime_check(100) == false );
+    REQUIRE( prime_check(7) == true );
 }
